881c3e699e412adeba803163ad90e1c4bed8fc14
Mukhan Myrzakulov <mukhan.m@gmail.com>
2013-08-28 13:57:22 +0100

    Added UnionFind algorithm.

---
+++ Graph/src/chapter1/UnionFind.java
@@ -0,0 +1,29 @@
+package chapter1;
+
+public class UnionFind {
+	private int [] id;
+	public UnionFind(int n) {
+		id = new int[n];
+		for(int i = 0; i < n; ++i) {
+			id[i] = i;
+		}
+	}
+	
+	private int find(int p) {
+		return 0;
+	}
+	
+	public void union(int p, int q) {
+		
+	}
+	
+	public boolean isConnected (int p, int q) {
+		return false;
+	}
+	
+	private int count() {
+		return 0;
+	}
+}
+
+
